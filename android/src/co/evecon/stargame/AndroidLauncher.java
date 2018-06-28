780a63fdbdbd356608ecf41b6f3dc565290d0f14
Borg <alex.kotyukov@yandex.ru>
2018-06-28 22:48:18 +0300

    First commit

---
+++ android/src/co/evecon/stargame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package co.evecon.stargame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import co.evecon.stargame.AlexStarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AlexStarGame(), config);
+	}
+}
---
+++ desktop/src/co/evecon/stargame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package co.evecon.stargame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import co.evecon.stargame.AlexStarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AlexStarGame(), config);
+	}
+}
