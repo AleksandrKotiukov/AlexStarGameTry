2bbc89bc7121079052186e0e98b92fda57cfbba5
Borg <alex.kotyukov@yandex.ru>
2018-07-06 18:11:33 +0300

    Third Lesson

--- core/src/co/evecon/stargame/screen/MenuScreen.java
+++ core/src/co/evecon/stargame/screen/MenuScreen.java
@@ -9,13 +9,14 @@ import com.badlogic.gdx.math.Vector2;
 
 import co.evecon.stargame.base.Base2DScreen;
 
-//Menu screen
+
+/**
+ * Экран меню
+ */
 
 public class MenuScreen extends Base2DScreen {
-    SpriteBatch batch;
+
     Texture img;
-    Vector2 pos, endPos, dir;
-    Vector2 v;
 
     public MenuScreen(Game game) {
         super(game);
@@ -24,13 +25,7 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
-        pos = new Vector2(0f,0f);
-        endPos = new Vector2(350f,200f);
-        dir = endPos.sub(pos);
-        v = new Vector2(dir.x/400,dir.y/400);
-        System.out.println(v);
     }
 
     @Override
@@ -38,24 +33,24 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        pos.add(v);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        batch.draw(img, -5f, -5f, 10f, 10f);
         batch.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
         img.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        endPos = new Vector2(screenX,Gdx.graphics.getHeight() - screenY);
-        dir = endPos.sub(pos);
-        v = new Vector2(dir.x/400,dir.y/400);
-        return super.touchUp(screenX, screenY, pointer, button);
+    public void touchDown(Vector2 touch, int pointer) {
+        super.touchDown(touch, pointer);
+    }
+
+    @Override
+    public void touchUp(Vector2 touch, int pointer) {
+        super.touchUp(touch, pointer);
     }
 }
